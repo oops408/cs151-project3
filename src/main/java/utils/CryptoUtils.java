3f07c9790b771d9fe1f6afd676ab5be77982b20f
soup <68081409+oops408@users.noreply.github.com>
2026-05-06 05:02:54 -0700

    Update CryptoUtils.java

--- src/main/java/utils/CryptoUtils.java
+++ src/main/java/utils/CryptoUtils.java
@@ -33,3 +33,28 @@ public final class CryptoUtils {
         }
     }
 }
+
+/* Original:
+package utils;
+
+import javax.crypto.Cipher;
+import java.util.Base64;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.SecretKey;
+
+public class EncryptionUtil {
+    private static SecretKey key = new SecretKeySpec("1234567890123456".getBytes(), "AES");
+
+    public static String encrypt(String text) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(text.getBytes()));
+    }
+
+    public static String decrypt (String text) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(text)));
+    }
+}
+*/
