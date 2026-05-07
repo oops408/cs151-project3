04904553058ceaf22aa5936343a671a534e4e332
Derek <derekyeh2004@gmail.com>
2026-05-07 09:57:34 -0700

    add SnakeApp

---
+++ src/main/java/snake/app/SnakeApp.java
@@ -0,0 +1,10 @@
+package snake.app;
+
+import snake.SnakeUI;
+import javafx.application.Application;
+
+public class SnakeApp {
+    public static void main(String[] args) {
+        Application.launch(SnakeUI.class, args);
+    }
+}
