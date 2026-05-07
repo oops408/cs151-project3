63752374141d5b5c1212664c0e4c4b7390d0d99d
MrHitaDavid3 <168222539+MrHitaDavid3@users.noreply.github.com>
2026-05-07 01:01:46 -0700

    Update Player.java
    Added more functions to aid with the player class

--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -1,17 +1,65 @@
 public class Player {
 
+    // Here are my variables
     private String name;
     private Hand hand;
     private int money;
     private int bet;
 
+    // This is my establishing a player
     public Player(String name, int startingMoney) {
         this.name = name;
         this.money = startingMoney;
         this.bet = 0;
         this.hand = new Hand();
     }
+
+    // Here is my function which I made to place bets
+    public void placeBet(int amount) {
+		
+        if (amount < 0) {
+			amount = 0;
+		}
+
+		if (amount > money) {
+			amount = money;
+		}
+
+		bet = amount;
+		money = money - amount;
+	}
+
+    // Here is the function which deals whenever you win
+	public void winBet() {
+		money = money + bet + bet;
+		bet = 0;
+	}
     
+
+    // Here is the function which deals whenever you lose
+	public void loseBet() {
+		bet = 0;
+	}
+
+    // Here is the function which pushes your bet, this only occurs whenever you tie with the dealer
+	public void pushBet() {
+		money = money + bet;
+		bet = 0;
+	}
+
+    // Here is my function to handle clearing the hand
+	public void clearHand() {
+		hand = new Hand();
+		bet = 0;
+	}
+
+    // This function's purpose is to save the text
+	public String saveText() {
+		return name + "," + money + "," + bet + "," + hand.saveText();
+	}
+
+
+    // I created the following getter methods for name, hand, money, and bet
     public String getName() {
         return name;
     }
